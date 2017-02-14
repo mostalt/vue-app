ea828c256c600286e91889f1e7427cf1fec8991c
mostalt <mostalt.mail@gmail.com>
2017-02-14 19:14:07 +0300

    add vuex and simple counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="counter-wrapper">
+    <div class="counter">
+      {{count}}
+    </div>
+    <button @click="$store.commit('INCREMENT')">Increment</button>
+    <button @click="$store.commit('DECREMENT')">Decrement</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      count () {
+        return this.$store.state.count
+      }
+    }
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="page">
+    <Counter/>
     <Hello/>
   </div>
 </template>
 
 <script>
   import Hello from '../components/Hello.vue'
+  import Counter from '../components/Counter.vue'
 
   export default {
     components: {
-      Hello
+      Hello,
+      Counter
     }
   }
 
