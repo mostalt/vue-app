e8f156bb3940c64bc292905fd517987a142b3e7c
mostalt <mostalt.mail@gmail.com>
2017-02-14 18:34:32 +0300

    changed basic architecture

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page">
+    <Hello/>
+  </div>
+</template>
+
+<script>
+  import Hello from '../components/Hello.vue'
+
+  export default {
+    components: {
+      Hello
+    }
+  }
+
+</script>
